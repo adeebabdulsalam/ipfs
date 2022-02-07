89acaf92a722a1ecf018acbdd149fec205ea070e
Adeeb Abdul Salam <adeebnabdulsalam@gmail.com>
2022-02-07 11:26:21 +0300

    contract tested and migrated

--- truffle-config.js
+++ truffle-config.js
@@ -5,8 +5,24 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
     }
-  }
+  },
+
+  compilers: {
+    solc: {
+       version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       }
+      //  evmVersion: "byzantium"
+      // }
+    }
+  },
 };
